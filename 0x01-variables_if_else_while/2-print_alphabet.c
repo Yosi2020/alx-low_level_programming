a3f52622703b2c94aa0f560e5d6642e10141fe54
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-03-11 23:21:26 +0300

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -18,25 +17,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-=======
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		putchar(c);
-		c++;
-	}
-	putchar('\n');
-	return (0);
-}
->>>>>>> 5c711f7f4150d675afed1b78c6b85d9af1e23047
