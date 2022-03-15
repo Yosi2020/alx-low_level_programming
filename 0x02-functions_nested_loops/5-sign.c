ec6577c1a3a5be851ff75f5e391b825b0bd7c470
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-03-16 01:20:22 +0300

    task 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,17 @@
 #include "main.h"
 /**
- * print_sign - prints the sign of a numver
+ * _abs - computes the absolute value of an integrer
  *
  * @n: the int to check
  *
- * Return: 1 and prints + if n is greater than zero
+ * Return: the absolute value of int
  *
- * 0 and prints 0 if n is zero
- *
- * -1 and prints - if n is less than zero
  */
-int print_sign(int n)
+int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
-		_putchar('+');
-		return (1);
+		return (n);
 	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-	}
-	return (-1);
+	return (-n);
 }
