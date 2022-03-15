11de5fd1c41f6feecee8f3b0bf7cb22cd5151430
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-03-16 00:40:02 +0300

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@
 
 void print_alphabet_x10(void)
 {
-int i = 0;
-while(i<10)
-{
-char eyu = 'a';
-while(eyu <= 'z')
-{
- _putchar(eyu);
-eyu++;
-}
-_putchar('\n');
-i++;
-}
+ int i = 0;
+ while(i<10)
+ {
+  char eyu = 'a';
+  while(eyu <= 'z')
+  {
+   _putchar(eyu);
+   eyu++;
+  }
+  _putchar('\n');
+  i++;
+ }
 }
