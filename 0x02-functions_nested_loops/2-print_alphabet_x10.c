e7910b78292c3d73f6bfb37ac9dab8be3892d861
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-03-16 00:40:55 +0300

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,24 @@
 #include "main.h"
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lower
+ * Return : Always return 0
  */
-
 void print_alphabet_x10(void)
 {
- int i = 0;
- while(i<10)
- {
-  char eyu = 'a';
-  while(eyu <= 'z')
-  {
-   _putchar(eyu);
-   eyu++;
-  }
-  _putchar('\n');
-  i++;
- }
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
