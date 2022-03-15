3d68e75d6c45b21bbfd8d93efd9d9219b7301bf6
Yosi2020 <eyosimar524@gmail.com>
2022-03-16 01:08:46 +0300

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - check the integer 
+ * 
+ * @n - checker 
+ */
+
+int print_sign(int n)
+{
+if (n>0)
+{ _putchar("+");}
+else if (n==0)
+{_putchar("-");}
+else{
+_putchar(-)}
+}
