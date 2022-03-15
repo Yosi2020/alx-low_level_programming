ba3f685faa72ef0df09ec45c96fef62f1272dfaa
Yosi2020 <eyosimar524@gmail.com>
2022-03-16 00:27:11 +0300

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+for ( int i = 0; i<10; i++)
+{
+char eyu = 'a';
+while(eyu <=z)
+{
+ _putchar(eyu);
+eyu++;
+}
+_putchar("\n");
+}}
