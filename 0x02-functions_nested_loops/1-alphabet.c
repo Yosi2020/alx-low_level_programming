838fd6cbafff4aa8f7b1c63e6a18cb07e4cb8724
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-03-16 00:08:38 +0300

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
-#include <main.h>
+#include "main.h"
 
-/*
- * main - check the code
- * 
- * Return: Always 0.
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
  */
-
-int print_alphabet (void)
+void print_alphabet(void)
 {
-char eyu;
-eyu = 'a';
-while(eyu <= "z"){
-_putchar(eyu);
-eyu++;
+	char ch;
 
-_putchar('/n');
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
 }
