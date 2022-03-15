d37104b5ae6139bff92d96b31bd288266cdcaf81
Yosi2020 <eyosimar524@gmail.com>
2022-03-16 00:02:49 +0300

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+
+/*
+ * main - check the code
+ * 
+ * Return: Always 0.
+ */
+
+int main (void)
+{
+char eyu;
+eyu = 'a';
+while(eyu <= "z"){
+_putchar(eyu);
+eyu++;
+
+return (0);
+}
