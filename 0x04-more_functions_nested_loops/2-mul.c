1ac408b2c7ee0c11cdd52366665b5826e897bdfa
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-03-21 21:01:39 +0300

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,13 @@
 
 /**
  * mul - input
+ * Description: multiplies 2 numbers
  * @a: first number
  * @b: second number
- * Return: void
+ * Return: the answer
  */
 
-int mul(int a int b)
+int mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
