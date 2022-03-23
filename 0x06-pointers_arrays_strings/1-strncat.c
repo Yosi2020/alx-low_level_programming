98a22e6f5e74d34f5db8df6f94428a690786c4ef
Yosi2020 <eyosimar524@gmail.com>
2022-03-23 15:01:38 +0300

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/** 
+ * _strncat - concatenates n numbers of characters from src to dest
+ * @dest: destination string
+ * @src: the souce tobe concatenated
+ * @n: the number of byte of src to be concatebated to dest
+ * Return: dest.
+ */
+
+char *strncat(char *dest, char *src, int n)
+{
+int len;
+char *ptr;
+
+for (len =0; dest[len] != '\0'; len++)
+;
+prt = dest + len;
+while (*src != '\0' && n--)
+*ptr++ = *src++;
+
+*ptr = '\0';
+return (dest);
+}
