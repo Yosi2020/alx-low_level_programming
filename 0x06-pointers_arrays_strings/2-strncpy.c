e3285b075e8c7782ba2b19a183380f7fac86f235
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-03-25 11:24:50 +0300

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,9 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-for ( int i = 0; src[i] != '\0' && i < n; i++)
-dest[i] = src[i];
-for (int i = 0; i< n; i++)
-dest[i] = '\0';
-return (dest);
+  for ( int i = 0; src[i] != '\0' && i < n; i++)
+    dest[i] = src[i];
+  for (int i = 0; i< n; i++)
+    dest[i] = '\0';
+  return (dest);
 }
