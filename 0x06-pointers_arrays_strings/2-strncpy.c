1a2471b894f99eac48cdaa6a7f58c419a9c52556
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-03-25 11:27:04 +0300

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string src to dest
- * strncpy() function
- * @dest: the varibale to strong copied string
- * @src: the string to be copied
- * @n: the number of bytes to be copied
+ * _strncpy - copies a string from src to dest. | Behaves exactly
+ * strncpy() function.
+ * @dest: the variable to store copied string.
+ * @src: the string  to be copied
+ * @n: the number of bytes to be copied.
  *
- * Return: dest
+ * Return: dest.
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-  for ( int i = 0; src[i] != '\0' && i < n; i++)
-    dest[i] = src[i];
-  for (int i = 0; i< n; i++)
-    dest[i] = '\0';
-  return (dest);
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
